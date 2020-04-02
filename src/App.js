0fe54611e89ffce7fa447b5b4d9d71552e25d389
github username <github email address>
2020-04-02 10:01:08 -0400

    not done

--- src/App.js
+++ src/App.js
@@ -1,37 +1,50 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import articles from './articles'
-import ArticleCard from './ArticleCard'
-import ArticleItem from './ArticleItem'
-
+import articles from './articles';
+import ArticleCard from './ArticleCard';
+import ArticleItem from './ArticleItem';
 
 class App extends React.Component {
-
   state = {
     articles: articles,
-  }
+    theme: 'light',
+    disabled: false
+  };
 
   renderArticles = () => {
     return this.state.articles.map(article => {
-      return <ArticleCard 
-              key={article.id} 
-              title={article.title}
-              url={article.url}
-              urlToImage={article.urlToImage}
-              description={article.description}
-              />
-    })
-  }
-
-  render(){
+      return (
+        <ArticleCard
+          key={article.id}
+          title={article.title}
+          url={article.url}
+          urlToImage={article.urlToImage}
+          description={article.description}
+        />
+      );
+    });
+  };
+  handleClick = () => {
+    let newDisabled;
+    let newTheme;
+    this.state.disabled ? (newDisabled = false) : (newDisabled = true);
+    this.state.theme === 'light' ? (newTheme = 'dark') : (newTheme = 'light');
+    this.setState({
+      ...this.state.articles,
+      disabled: newDisabled,
+      theme: newTheme
+    });
+  };
+  render() {
     return (
-      <div className="light">
-        <button>Switch to Dark Mode</button>
-        <button>Switch to List View</button>
-        <div className="cards">
-          {this.renderArticles()}
-        </div>
+      <div className={this.state.theme}>
+        {this.state.disabled ? (
+          <button onClick={this.handleClick}>Switch to Dark Mode</button>
+        ) : (
+          <button onClick={this.handleClick}>Switch to List View</button>
+        )}
+        <div className='cards'>{this.renderArticles()}</div>
       </div>
     );
   }
--- src/ArticleCard.js
+++ src/ArticleCard.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from 'react';
+import ArticleItem from './ArticleItem';
 
-function ArticleCard(props){
+function ArticleCard(props) {
   return (
-      <div className="card light">
-        <h4>{props.title}</h4>
-        <div className="image-wrapper">
-          <img className="image" src={props.urlToImage}/>
-        </div>
-        <p>{props.description}</p>
-      <a href={props.url} target="_blank">Click for full article...</a>
+    <div className='card light'>
+      <h4>{props.title}</h4>
+      <div className='image-wrapper'>
+        <img className='image' src={props.urlToImage} />
       </div>
-  )
+      <p>{props.description}</p>
+      <a href={props.url} target='_blank'>
+        Click for full article...
+      </a>
+    </div>
+  );
 }
 
-export default ArticleCard
+export default ArticleCard;
